baa79bbde972ac405e64d3d7f1020d505da3f15c
Duy Pham <ericpham19@outlook.com>
2022-10-02 17:12:21 +1100

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_accessor :Knowledge
+
+    def initialize
+        @knowledge= []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
